e7ea5547262cc964d6970bc78c6c5d60738a4282
zhaoyuzhi <531708015@qq.com>
2023-03-10 18:47:19 +0800

    add

--- scripts/sample_folder_vimeo90k.py
+++ scripts/sample_folder_vimeo90k.py
@@ -1,5 +1,4 @@
 import os
-import cv2
 import tqdm
 from PIL import Image
 from clip_interrogator import Config, Interrogator
@@ -14,6 +13,6 @@ for i in range(1, 97):
         for k in range(1, 8):
             imgpath = os.path.join(folderpath, str(i).zfill(5), str(j).zfill(4), 'im' + str(k) + '.png')
             image = Image.open(imgpath).convert('RGB')
-            image = cv2.resize(image, (896, 512))
+            image = image.resize((896, 512))
             print(imgpath)
             print(ci.interrogate_best(image))
